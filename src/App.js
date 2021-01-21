ad9a0c82f3901d42bdf07110d1922fd6e099680d
Oleg Strokan <oleg14ua71@gmail.com>
2021-01-21 22:32:44 +0100

    Version 1.0 Youtube-API Project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route} from "react-router-dom";
-import UsersContainer from "./component/Users/UsersContainer";
-import ProfileContainer from "./component/Profile/ProfileContainer";
-import StartContainer from "./component/Start/StartContainer";
-import Login from "./component/Login/Login";
+
 
 const App = () => {
     return <div className='app-wrapper'>
-        <Route exact path='/'
-               render={ () => <StartContainer /> }/>
-            <Route path='/users'
-                   render={ () => <UsersContainer /> }/>
-        <Route path='/profile/:userId?'
-               render={() => <ProfileContainer/>}/>
-        <Route path='/login'
-               render={ () => <Login /> }/>
+Youtube Project
     </div>
 }
 
