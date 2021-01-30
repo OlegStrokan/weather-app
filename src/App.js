3c5d81fc5b106aa55f11e5ca65509852f02681f3
Oleg Strokan <oleg14ua71@gmail.com>
2021-01-30 22:45:46 +0100

    Weather app 1.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import WeatherDisplay from './components/WeatherDisplay/WeatherDisplay';
 
+const PLACES = ["London", "Moscow", "Kiev", "Minsk"];
 
-const App = () => {
-    return <div className='app-wrapper'>
-Youtube Project
-    </div>
+export default class App extends Component {
+    state = {
+        activePlace: 0,
+    };
+
+    render() {
+        const activePlace = this.state.activePlace;
+        return (
+            <div className="App">
+
+                {PLACES.map((place, index) => (
+                    <button
+                        key={index}
+                        onClick={() => {
+                            this.setState({activePlace: index});
+                        }}>
+                        {place}
+                    </button>
+                ))}
+
+                <WeatherDisplay
+                    key={activePlace}
+                    name={PLACES[activePlace]}/>
+            </div>
+        );
+    }
 }
-
-export default App;
