96c45d6120170e85aadade4ae1821e9688ad39f2
Oleg Strokan <oleg14ua71@gmail.com>
2021-02-23 18:04:16 +0100

    heroku-commit

--- src/components/Documentation/Documentation.js
+++ src/components/Documentation/Documentation.js
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 const Documentation = (props) => {
     return <div className={s.app}>
         <div className={s.documentation}>
-        <h2>This is documentation</h2>
+        <h2>Documentation</h2>
             <div className={s.mainText}>
                 This weather app shows you current weather in any city. By default, I set 2 cities.
             But you can also add the city name in the field below. <p>REST API: <a href="https://openweathermap.org/current">https://openweathermap.org/current</a></p>
